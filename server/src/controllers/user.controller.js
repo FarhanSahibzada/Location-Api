ec0bf1bd0c2308874167c752e1085fef6fdd6b1f
Farhan Sahibzada <farhansahabzada3@gmail.com>
2025-08-02 01:41:31 +0500

    work on the finding distance-between_coordinates function

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { User } from '../Schema/user.modal.js';
 import { asyncHandler } from '../utlis/asynchandler.js';
-import { ApiError } from '../utlis/ErrorApi.js';
+import { ApiError } from '../utlis/ErrorApi.js(';
 import { responseApi } from '../utlis/responseApi.js';
 
 // sending coordintes and getting the exact location with the help of opencage
@@ -22,12 +22,39 @@ const reverse_geocoding = async (lat, lng) => {
 }
 
 //  checking if the user who login is near the user refgister coordinates
-const checking_coordinates_tolerance = async()=>{
+const distance_ = async (current_lat, current_lng, reg_lat, reg_lng) => {
     try {
+        const r = 6371000;
+        const change_to_rad = (deg) => deg * Math.PI / 180;
+        const dlat = change_to_rad(current_lat - reg_lat);
+        const dlng = change_to_rad(current_lng, reg_lng);
+        const current_lat_rad = change_to_rad(current_lat);
+        // const current_lng_rad = change_to_rad(current_lng);
+        const register_lat_rad = change_to_rad(reg_lng);
+        // const register_lng_rad = change_to_rad(reg_lng);
+
+        console.log("checking dlat=", dlat);
+        console.log("checking dlng", dlng);
+        console.log("checking lat_rad", current_lat_rad);
+        // console.log("checking lng_rad", current_lng_rad);
+        console.log("checking reg_lat", register_lat_rad);
+        // console.log("checking reg_lng", register_lng_rad);
+
+        const a = Math.sin(dlat / 2) * Math.sin(dlat / 2) +
+            Math.cos(current_lat_rad) * Math.cos(register_lat_rad) *
+            Math.cos(dlng / 2) * Math.cos(dlng / 2);
+
+        console.log("checking a after half haversine formula= ", a);
+
+        const c = Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        console.log("checking c after most of the  haversine formula= ", c);
+
+        const distance = r * c;
+        console.log(distance);
 
     } catch (error) {
-        throw new ApiError(500,"error while checking the coordinates tolerance");
-        
+        throw new ApiError(500, "error while checking the coordinates tolerance");
+
     }
 }
 
@@ -92,25 +119,42 @@ const login = asyncHandler(async () => {
     const { email } = req.body;
 
     if (!email) {
-        throw new ApiError(401, "can not the email");
+        throw new ApiError(403, "can not get the email form frontend");
     }
 
     const finduser = await User.findOne({ email });
     if (!finduser) {
-        throw new ApiError(404, "can not find the user ");
+        throw new ApiError(404, "email is not found please try with the correct email");
     }
 
+    const rawIP =
+        req.headers["x-forwarded-for"]?.split(',')[0] ||
+        req.connection?.remoteAddress || "8.8.8.8";
+
+    const response = await fetch(`https://ipwho.is/${rawIP}`);
+    const data = await response.json();
+
+    if (data.success == false) {
+        throw new ApiError(500,"something went please try again later");
+    }
+
+     const current_lat = data?.latitude ;
+     const current_lng =  data?.longitude;
+     const reg_lat = finduser?.coords.lat;
+     const reg_lng = finduser?.coords.lng;
+    
     /**
  * Checks if user is within 50 meters of their registration location.
  * 
- * @param {number} currentLat - The user's current latitude.
- * @param {number} currentLng - The user's current longitude.
- * @param {number} regLat - The latitude where the user registered.
- * @param {number} regLng - The longitude where the user registered.
+ * @param {number} current_lat- The user's current latitude.
+ * @param {number} current_lng - The user's current longitude.
+ * @param {number} reg_lat - The latitude where the user registered.
+ * @param {number} reg_lng  - The longitude where the user registered.
  * @returns {boolean} True if within 50 meters.
  * 
  * @author Farhan Sahibzada
  */
+
 
     const actuall_address = await reverse_geocoding();
     console.log("checking the exact location where someone is access user account ==", actuall_address);
